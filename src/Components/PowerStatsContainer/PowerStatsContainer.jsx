c2781e2f004bbf2046cad178a8ddf17cfea9e0de
Agustin <agustinrossello@gmail.com>
2022-01-05 19:59:00 -0300

    first commit

--- src/Components/PowerStatsContainer/PowerStatsContainer.jsx
+++ src/Components/PowerStatsContainer/PowerStatsContainer.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import PowerStats from '../PowerStats/PowerStats'
 
 
 
 const PowerStatsContainer = () => {
+
+    // const url = '';
+
+    // const [heroes, setHeroes] = useState([]);
+
+    // useEffect(() => {
+    //    axios.get(url).then(resp => {
+    //        setHeroes(resp.data);
+    //    });
+
+    // }, []);
+
+
     return (
         <div className="powerstats-list-container">
             <div className="powerstats-team-container">
